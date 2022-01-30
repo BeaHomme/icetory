eef63d8e847ae0c600852a5f6a0fe5285209549d
Alina Gladkova <a-g1997@yandex.ru>
2022-01-30 13:12:37 +0500

    Feedback section edited

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-const swiper = new Swiper('.stories__swiper', {
+'use strict';
+
+const swiperStories = new Swiper('.stories__swiper', {
   slidesPerView: 4,
+  freeMode: true,
+});
+const swiperFeedback = new Swiper('.feedback__swiper', {
+  slidesPerView: 1,
+  slidePerGroup: 1,
+  centeredSlides: true,
+  // loop: true,
+  initialSlide: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.feedback__next',
+    prevEl: '.feedback__prev',
+  },
 });
